c26107264ca4629bf9cd5e96803ec27da0de7e1b
NIKHITA PARDHE <153489800+Nikhita-55@users.noreply.github.com>
2024-09-18 17:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name
+this.age=age
+	}
+get name(){
+	return this.name;
+}
+	set age(age){
+		if(age>0)
+			this.age=age;
+	}
+	else {
+            throw new Error("Age must be positive.");
+        }
+    }
 
-class Student extends Person {}
+    get age() {
+        return this.age;
+    }
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	 study() {
+        console.log(`${this.name} is studying.`);
+    }
+}
+
+class Teacher extends Person {
+	  teach() {
+        console.log(`${this.name} is teaching.`);
+    }
+}
+const student = new Student("Alice", 20);
+student.study();  // Output: Alice is studying.
+
+const teacher = new Teacher("Mr. Smith", 40);
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
